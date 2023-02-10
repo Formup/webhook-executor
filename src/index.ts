339bb7be7bd3e5efb42b7e3c6aa517f2372b1019
CABBY <s2001055@edu.bc.fi>
2023-02-10 16:14:32 +0200

    scripts added

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,32 @@
 import 'dotenv/config';
 import express from 'express';
+import { exec } from 'child_process';
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.get('/webhooks', (req, res) => {
+    let fileType = '.bat';
+
+    if (process.platform !== 'win32') {
+        fileType = '.sh';
+    }
+
+    exec(
+        `${__dirname}/scripts/test-echo${fileType}`,
+        (error, stdout, stderr) => {
+            if (error) {
+                console.error(error.message);
+                return;
+            }
+
+            if (stderr) {
+                console.error(stderr);
+                return;
+            }
+
+            res.status(200).send(stdout);
+        }
+    );
 });
 
 const PORT = process.env.PORT || 5000;
