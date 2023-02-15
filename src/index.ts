f55fdd639003d6f345a0a5033208cbf4947aa31e
s2001055 <s2001055@edu.bc.fi>
2023-02-15 16:51:36 +0200

    console.log deleted

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,6 @@ import hooksRouter from './routes/hooks';
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-console.log('testing server11...');
-
 app.use(express.json());
 app.use('/hooks', hooksRouter);
 
