f999223218754ddac5c88932e4292c0e67b2b326
s2001055 <s2001055@edu.bc.fi>
2023-02-16 10:39:48 +0200

    testing

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 5000;
 console.log('Testing server...');
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use('/log-github-webhook', hooksRouter);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
