249c3de1f2781fddec5d038d3c96cd528add8bc2
s2001055 <s2001055@edu.bc.fi>
2023-02-16 09:30:37 +0200

    testing server

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import hooksRouter from './routes/hooks';
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-console.log('Testing...');
+console.log('Testing....');
 
 app.use(express.json());
 app.use('/hooks', hooksRouter);
