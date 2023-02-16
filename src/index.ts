8ba03f26524c8f20d521714375cd9dd3d8ea584d
s2001055 <s2001055@edu.bc.fi>
2023-02-16 10:05:55 +0200

    testing

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,7 @@ import hooksRouter from './routes/hooks';
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-console.log('Testing.....');
-
 app.use(express.json());
-app.use('/hooks', hooksRouter);
+app.use('/log-github-webhook', hooksRouter);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
